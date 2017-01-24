f5fa3f9c65b8fa70855b421fa6f2f779153e87ba
hsuyuming <abego452@gmail.com>
2017-01-24 16:07:01 +0800

    16-07-01

---
+++ 2017-01-22_location_name.py
@@ -0,0 +1,97 @@
+# -*- coding: utf-8 -*-
+
+import codecs
+import json
+
+# # ＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝
+# # ＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝
+# version1
+# 讀取JSON檔
+# 錯誤檢討：
+#   會有Error的情形是因為，宇辰給的fb_address_1336.json檔案內的格式不相符，每個dist後面沒有逗點，所以造成程式在讀取檔案的時候，發生
+# 錯誤
+# # ＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝
+# # ＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝
+
+# with codecs.open('fb_address_1336.json', 'rb','utf-8') as f:
+#     # print f #<open file 'fb_address_1336.json', mode 'rb' at 0x1004c6a50>
+#     data = json.load(f)
+# # print data
+
+# # ＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝
+# # 錯誤紀錄
+# /Users/mac/anaconda/bin/python /Users/mac/PycharmProjects/untitled1/project/2017-01-22_location_name.py
+# Traceback (most recent call last):
+#   File "/Users/mac/PycharmProjects/untitled1/project/2017-01-22_location_name.py", line 9, in <module>
+#     data = json.load(f)
+#   File "/Users/mac/anaconda/lib/python2.7/json/__init__.py", line 291, in load
+#     **kw)
+#   File "/Users/mac/anaconda/lib/python2.7/json/__init__.py", line 339, in loads
+#     return _default_decoder.decode(s)
+#   File "/Users/mac/anaconda/lib/python2.7/json/decoder.py", line 367, in decode
+#     raise ValueError(errmsg("Extra data", s, end, len(s)))
+# ValueError: Extra data: line 2 column 1 - line 1337 column 1 (char 700 - 765909)
+#
+# Process finished with exit code 1
+# # ＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝
+
+
+
+
+# # ＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝
+# # ＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝
+# version2
+# 想法：將fb_address_1336.json檔案內每一行的dist後面加上『,』，並加入條件判斷，讓第一行的頭多加一個『 [ 』，最後一
+# 行刪除最後一個『 , 』並且多加一個『 ] 』。
+# 結果：成功
+# # ＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝
+# # ＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝
+
+myfile=codecs.open('fb_address_1336.json','rb','utf-8')
+readfiles=myfile.readlines()
+z=0
+with codecs.open('fb_address_1336_new.json',"w",'utf-8') as location_name:
+    for x in readfiles:
+        z=z+1
+        if z ==1 :
+            location_name.write('['+x.strip() + ',')
+        if z>1 and z < len(readfiles):
+            location_name.write(x.strip() + ',')
+        if z == len(readfiles):
+            location_name.write(x.strip()+']')
+
+with codecs.open('fb_address_1336_new.json', 'rb','utf-8') as f:
+    data = json.load(f)
+for i in data:
+    print i['name']
+
+
+# # # ＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝
+# # # ＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝
+# # version3
+# # 將version2所得到的結果存取成一個txt檔案
+# # # ＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝
+# # # ＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝
+#
+# myfile=codecs.open('fb_address_1336.json','rb','utf-8')
+# readfiles=myfile.readlines()
+# z=0
+# with codecs.open('fb_address_1336_new3.json',"w",'utf-8') as location_name:
+#     for x in readfiles:
+#         z=z+1
+#         if z ==1 :
+#             location_name.write('['+x.strip() + ',')
+#         if z>1 and z < len(readfiles):
+#             location_name.write(x.strip() + ',')
+#         if z == len(readfiles):
+#             location_name.write(x.strip()+']')
+# with codecs.open('project_location_name.txt',"w",'utf-8') as location_name:
+#     with codecs.open('fb_address_1336_new3.json', 'rb','utf-8') as f:
+#         data = json.load(f)
+#         for i in data:
+#             location_name.write(i['name'] + '\n')
+
+
+
+
+
